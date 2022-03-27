aba7bffb852f8b317683fcd735e28730302962b4
Xiaoyuan Du <duxy1@shanghaitech.edu.cn>
2022-03-27 14:53:03 +0800

    完成总数据清洗

--- DataClean/overallData.py
+++ DataClean/overallData.py
@@ -2,6 +2,7 @@
 import json
 
 import pandas as pd
+
 path = "../DataCrawl/rawdata/china_daily.json"
 store_path = "C:/Users/duxiaoyuan/Desktop/OnWorking/CS131/Project/DataClean/processeddata/overall.csv"
 with open(path, "r") as f:
@@ -14,3 +15,17 @@ for column in columns_to_be_deleted:
 raw_data['date'] = raw_data['y'] + '.' + raw_data['date']
 del raw_data['y']
 
+path = "../DataCrawl/rawdata/china_add_daily.json"
+with open(path, "r") as f:
+    data = json.loads(f.read().replace("\'", "\""))
+raw_add_data = pd.DataFrame(data)
+columns_to_be_deleted = ['deadRate', 'healRate', 'importedCase', 'dead', 'heal', 'suspect', 'localinfectionadd']
+for column in columns_to_be_deleted:
+    del raw_add_data[column]
+del raw_add_data['y'], raw_add_data['date']
+raw_add_data.rename(columns={'confirm': 'confirmadd', 'infect': 'noInfectadd'}, inplace=True)
+overall_data = pd.concat([raw_data, raw_add_data], axis=1)
+column_order = ["date", "localConfirm", "confirm", "nowConfirm", "noInfect", "localConfirmadd", "noInfectadd",
+                 "confirmadd"]
+overall_data = overall_data[column_order]
+overall_data.to_csv(store_path)
